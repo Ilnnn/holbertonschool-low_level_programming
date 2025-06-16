538cc5d84761d94fda7e40b5da672e086d1d357d
Ilnnn <camoreze@gmail.com>
2025-06-16 10:31:43 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,10 @@
 */                                                                                                                                 
 int main(void)
 {
-  Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/$ ./size64
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
+printf("Size of a char: %1d byte(s)\n", sizeof(char));
+printf("Size of an int: %1d byte(s)\n", sizeof(int));
+printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %1d byte(s)\n", sizeof(float));
 return (0);
 }
