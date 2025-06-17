a609e8fac70e733e4205dbe8f0e3b7a2d2ad1c40
Ilnnn <camoreze@gmail.com>
2025-06-17 14:33:09 +0000

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0' + x);
+		if (x < 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
