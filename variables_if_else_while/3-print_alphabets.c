df806b1ffec5461203e7aa5c10760565d03e741a
Ilnnn <camoreze@gmail.com>
2025-06-17 08:40:19 +0000

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main (void)
+/**
+ * main - Prints the lowercase and uppercase alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
 	putchar (low);
 
-	for (low= 'A'; low <= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar (low);
 	putchar ('\n');
 
