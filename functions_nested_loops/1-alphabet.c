850f03fe402e4be29a1f42181ec0026b5400a431
Ilnnn <camoreze@gmail.com>
2025-06-18 10:37:06 +0000

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+void print_alphabet(void)
+
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++) 
+{
+	putchar (l);
+	putchar ('\n');
+}
+	return;
+}
