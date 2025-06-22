b261790d62f9ce0c52ab4992edf2a353352209ad
Ilnnn <camoreze@gmail.com>
2025-06-22 12:00:46 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Affiche le dernier chiffre d'un entier
+ * @x: L'entier dont on veut afficher le dernier chiffre
+ *
+ * Return: Le dernier chiffre de l'entier
+ */
+int print_last_digit(int x)
+
+{
+	int a;
+
+	a = abs(x % 10);
+	_putchar(a + '0');
+	return (a);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-
+#include <stdlib.h>
 int _islower(int c);
 int _putchar(char c);
 int _isalpha (int c);
 int print_sign(int n);
 int _abs(int a);
+int print_last_digit(int x);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 
