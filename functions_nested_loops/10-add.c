6dd9f163dbd92c16e68d75ab17e4190c0cd3893d
Ilnnn <camoreze@gmail.com>
2025-06-22 18:52:19 +0000

    10-add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * add - Additionne deux entiers
+ * @s: Premier entier
+ * @ss: Deuxième entie
+ *
+ * Return: La somme des deux entiers
+ */
 int add(int s, int ss)
 	{
 	int x = s + ss;
