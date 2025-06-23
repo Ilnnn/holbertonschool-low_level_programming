f20eb7e653bc9e7fb130e12e77d6c726c8072267
Ilnnn <camoreze@gmail.com>
2025-06-23 09:27:54 +0000

    1-isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _isdigit - Vérifie si un caractère est un chiffre (0 à 9).
+ * @c: Le caractère à vérifier (code ASCII).
+ *
+ * Return: 1 si c est un chiffre, 0 sinon.
+ */
 int _isdigit(int c)
 {
 	{
