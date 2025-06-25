f80d324f2aadd3c1c16bfb6084baea0b24537a4e
Ilnnn <camoreze@gmail.com>
2025-06-25 11:45:18 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * reset_to_98 - Modifie la valeur d'un entier pointé98
+ * Pointeur vers un entier à mettre à jour
+ * Description: Maj la valeur de l'entier pointé par@n pour qu'elle devienne 9
+ * Return: Rien (void)
+ */
 int main(void);
 void reset_to_98(int *n)
 	{
