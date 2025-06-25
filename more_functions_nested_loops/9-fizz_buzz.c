18359e6532a1dd9f7f1fcd6a038f35758f976add
Ilnnn <camoreze@gmail.com>
2025-06-25 08:38:55 +0000

    9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Programme FizzBuzz de 1 à 100
+ *
+ * Description: Affiche les nombres de 1 à 100.
+ * Pour les multiples de 3, affiche "Fizz" à la place du nombre.
+ * Pour les multiples de 5, affiche "Buzz".
+ * Pour les multiples des deux, affiche "FizzBuzz".
+ * Les valeurs sont séparées par un espace.
+ *
+ * Return: 0 (succès)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+	{
+			printf("FizzBuzz");
+	}
+		else if	(i % 3 == 0)
+	{
+			printf("Fizz");
+	}
+		else if (i % 5 == 0)
+	{
+			printf("Buzz");
+	}
+		else
+	{
+			printf("%d", i);
+	}
+		if (i != 100)
+	{
+			printf(" ");
+	}
+	}
+			printf("\n");
+			return (0);
+}
