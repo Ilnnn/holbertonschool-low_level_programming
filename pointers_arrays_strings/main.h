de3e73661e19e59b9fd1e453583a66dc5d49479a
Ilnnn <camoreze@gmail.com>
2025-06-30 10:37:47 +0000

    0-memset

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 int _atoi(char *s);
 #endif
