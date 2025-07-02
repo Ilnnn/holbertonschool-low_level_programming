6a836326385d14c0c184e76be112212b33a82c9d
Ilnnn <camoreze@gmail.com>
2025-07-02 08:49:25 +0000

    5-strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strstr - Locates a substring
+ * @haystack: The string to search in
+ * @needle: The substring to find
+ *
+ * Return: A pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int h, n;
