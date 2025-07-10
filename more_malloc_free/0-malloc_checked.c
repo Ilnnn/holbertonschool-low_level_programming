ad8b5b5491cd327fb3bd5a3ca54e4273f60addaf
Ilnnn <camoreze@gmail.com>
2025-07-10 15:37:20 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * malloc_checked - Alloue mémoire avec mallc vérifie s iallocation réuss
+ * @b: Taille en octets à allouer
+ *
+ * Return: Pointeur vers la mémoire allouée
+ *         Termine le programme avec le code d'état 98 si malloc échoue
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
