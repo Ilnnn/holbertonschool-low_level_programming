0235b2eea129110745b997ed7518f98211f5a345
Ilnnn <camoreze@gmail.com>
2025-07-11 09:55:18 +0000

    0-main.c

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_print(struct dog *d);
+#endif
