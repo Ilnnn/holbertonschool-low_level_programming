110a714697398bdfedd31ea155a777b869e1ec42
Ilnnn <camoreze@gmail.com>
2025-07-16 12:02:35 +0000

    tt

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,39 +2,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * op_add - returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a + b
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_add - returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a - b
+ */
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_add - returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a * b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - returns the result of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a / b
+ *
+ * Note: If b is 0, this function may cause a division by zero error.
+ * Ensure proper validation before calling this function.
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
+/**
+ * op_mod - returns the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a % b
+ *
+ * Note: If b is 0, this function may cause a modulo by zero error.
+ * Ensure proper validation before calling this function.
+ */
+
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
