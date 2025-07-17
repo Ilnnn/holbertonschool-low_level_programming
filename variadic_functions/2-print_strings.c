89129527e731170ed068b33f3cae9a13f1958436
Ilnnn <camoreze@gmail.com>
2025-07-17 17:45:13 +0000

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,7 +1,11 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * print_strings - Imprime une string de caractere.
+ *@separator: pointeur sur une string.
+ *@n: Nombres de chaines de caractere.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 
 {
@@ -9,9 +13,9 @@ va_list strings;
 char *str;
 unsigned int index;
 
-va_start(strings, n);
+	va_start(strings, n);
 
-for (index = 0; index < n; index++)
+	for (index = 0; index < n; index++)
 
 	{
 		str = va_arg(strings, char *);
@@ -24,7 +28,7 @@ for (index = 0; index < n; index++)
 		if (index != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
-	
+
 	printf("\n");
 
 	va_end(strings);
