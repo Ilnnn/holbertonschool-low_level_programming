89dc9e3953a70d31ae90b25d9f4486e6735c9dba
Ilnnn <camoreze@gmail.com>
2025-07-28 09:52:39 +0000

    test1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * list_len -returns the number of elements in a linked list
- * @h : pointer to the list_h list
+ * list_len - Returns the number of elements in a linked list
+ * @h: Pointer to the list_t list
  *
- * return : number of elements in h 
+ * Return: Number of elements in h
  */
 size_t list_len(const list_t *h)
 {
