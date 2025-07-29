499b3316ebfb79d4a6c57f11f259e2d4791c1ded
Ilnnn <camoreze@gmail.com>
2025-07-29 10:53:32 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - Affiche tous les éléments d'une liste doue chaînée
+ * @h: pointeur vers le début de la liste
+ *
+ * Return: nombre total de nœuds
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - retourne la longueur d’une liste doublement chaînée
+ * @h: pointeur vers le début de la liste
+ * Return: nombre de nœuds
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t num = 0;
