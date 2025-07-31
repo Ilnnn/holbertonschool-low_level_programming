897ce97fbc24554a0a61b2c4f04677e971e756b1
Ilnnn <camoreze@gmail.com>
2025-07-31 12:36:23 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 /**
  * print_list - Prints all the elements of a list_t linked list
  * @h: Pointer to the head of the list
- *listint_s : test test test 
+ *listint_s : test test test
  * Return: The number of nodes in the list
  */
 size_t print_list(const list_t *h)
