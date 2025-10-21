3b13273ac9997e6458a2d9087dbd17b7f4023226
Ilnnn <camoreze@gmail.com>
2025-10-21 13:39:56 +0000

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -8,7 +9,7 @@ int main(void)
 	int n, sn;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	n = sn % 10;
+	sn = n % 10;
 	printf ("The last digit of the number %d is %d \n", n, sn);
 
 	return (0);
