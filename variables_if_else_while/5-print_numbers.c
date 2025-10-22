69d98ad51911d53530321e5ea199f13fd3623148
Ilnnn <camoreze@gmail.com>
2025-10-22 08:36:44 +0000

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10 starting from 0,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+
+	{
+		printf("%d", d);
+		d++;
+	}
+
+	printf("\n");
+	return (0);
+}
