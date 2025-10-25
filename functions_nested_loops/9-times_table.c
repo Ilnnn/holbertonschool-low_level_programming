60520050512edb0b43757b053906808d4c9ff7c9
Ilnnn <camoreze@gmail.com>
2025-10-25 23:45:19 +0000

    tt

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,41 +4,44 @@
  */
 void times_table(void)
 {
-	int ord, abs, p;
+	int ord;
+	int abs;
+	int p;
 
 	for (ord = 0; ord < 10; ord++)
+{
+		for (abs = 0; abs < 10; abs++)
 	{
-	for (abs = 0; abs < 10; abs++)
-	{
-	p = ord * abs;
-
-	if (abs == 0)
+		p = ord * abs;
+		if (abs == 0)
 	{
 		if (p < 10)	
-		_putchar(p + '0');
+			_putchar(p + '0');
 		else
 		{
-		_putchar((p / 10) + '0');
-		_putchar((p % 10) + '0')
+			_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
 		}
-	else
+		}
+	}	
+		else
 	{
 		if (p < 10)
 		{
-		_putchar(' ');
-		_putchar(p + '0');
+			_putchar(' ');
+			_putchar(p + '0');
 		}
 		else
 		{
-		_putchar(' ');
-		_putchar((p / 10) + '0');
-		_putchar((p % 10) + '0');
+			_putchar(' ');
+			_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
 		}
 		}
-	    if (abs < 9)
+	    	if (abs < 9)
 		{
-		_putchar(',');
-		_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
