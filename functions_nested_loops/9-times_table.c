8be82224fad097495d5f7aafdc440cbc7a9630fe
Ilnnn <camoreze@gmail.com>
2025-10-26 22:24:54 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,46 +4,36 @@
  * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
+
 {
 int ord;
 int abs;
 int p;
 
 	for (ord = 0; ord < 10; ord++)
-		{
-			for (abs = 0; abs < 10; abs++)
+	{
+		for (abs = 0; abs < 10; abs++)
 	{
 		p = ord * abs;
 		if (abs == 0)
 	{
-		if (p < 10)
 		_putchar(p + '0');
-	else
-		{
-			_putchar((p / 10) + '0');
-			_putchar((p % 10) + '0');
-		}
 	}
+	else if (p < 10)
+{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(p + '0');
+}
 	else
 	{
-		if (p < 10)
-		{
-			_putchar(' ');
-			_putchar(p + '0');
-		}
-	else
-		{
-			_putchar(' ');
-			_putchar((p / 10) + '0');
-			_putchar((p % 10) + '0');
-		}
+		_putchar(',');
+		_putchar(' ');
+		_putchar((p / 10) + '0');
+		_putchar((p % 10) + '0');
 	}
-		if (abs < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
 	}
-		_putchar('\n');	
-		}
+		_putchar('\n');
+	}
 }
