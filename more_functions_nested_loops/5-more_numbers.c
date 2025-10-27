70a7b55b790805f528ef146230b7f3c41328fc90
Ilnnn <camoreze@gmail.com>
2025-10-27 18:18:05 +0000

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - function
+ *
+ */
+
+	void more_numbers(void)
+{
+	int d;
+	int u;
+
+	for (d = 0; d < 10; d++)
+
+		for (u = 0; u <= 14; u++)
+		{
+			if (u >= 10)
+			_putchar('0' + u / 10);
+			_putchar('0' + u % 10);
+		}
+	_putchar('\n');
+}
