3ffe948f851da4d42981140eb0b802d7a06b953d
Ilnnn <camoreze@gmail.com>
2025-10-27 17:39:43 +0000

    3-print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_numbers - print digits from 0 to 9
+ * num: variable
+ *
+ */
 	void print_numbers(void)
-{	
+{
 	int c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 		putchar('\n');
-}	
+}
