a33e54083802299f8fe34272d708f4e0baae450c
Ilnnn <camoreze@gmail.com>
2025-10-30 22:54:01 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: difference between first non-matching characters, or 0 if equal
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
