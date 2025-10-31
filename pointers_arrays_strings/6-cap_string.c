08e9dde11d7995e1dfc511816acb75ed503ec345
Ilnnn <camoreze@gmail.com>
2025-10-31 11:38:59 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+	char spe[] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
+	int nw = 1;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (spe[j] != '\0')
+		{
+			if (s[i] == spe[j])
+		{
+			nw = 1;
+			break;
+		}
+		j++;
+		}
+		if (nw == 1 && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+			nw = 0;
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			nw = 0;
+		}
+	i++;
+}
+	return (s);
+}
