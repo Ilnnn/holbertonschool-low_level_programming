47e3edfa1ef4587aec7c8f5c07b00bf7686346dd
Ilnnn <camoreze@gmail.com>
2025-10-31 09:15:39 +0000

    4-rev_array and 5-string

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @chaine: string to modify
+ *
+ * Return: pointer to the modified string
+ */
 char *string_toupper(char *chaine)
 {
 	int i = 0;
