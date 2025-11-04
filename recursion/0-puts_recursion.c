35238eb4f3c7e7126c1aa0d6c034df410acc8a80
Ilnnn <camoreze@gmail.com>
2025-11-04 10:34:26 +0000

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _puts_recursion - Affiche chaîne de caractères suivie d'une new ligne
+ * @s: Pointeur vers la chaîne à afficher
+ */
 void _puts_recursion(char *s)
 {
-	if == (*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		return
+		return;
 	}
 	_putchar(*s);
-	_putchar(s + 1);
+	_puts_recursion(s + 1);
 }
