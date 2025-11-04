c206bcab804150ac8624142764872eb7760572f7
Ilnnn <camoreze@gmail.com>
2025-11-04 10:50:45 +0000

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Affiche une chaîne de caractères à l'envers
+ * @s: Pointeur vers la chaîne à afficher
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\0')
+	return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(s[0]);
+}
