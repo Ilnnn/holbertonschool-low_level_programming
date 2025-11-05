d1c60511b658408f76a73fcf43832f41876fd436
Ilnnn <camoreze@gmail.com>
2025-11-05 13:55:41 +0000

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,21 +8,18 @@
  *
  * Return: 0 on success, 1 if arguments are invalid
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int n1, n2, result;
-	
+
 	if (argc != 3)
 	{
-		printf("Error\n")
+		printf("Error\n");
 		return (1);
 	}
 
-	else
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
 
 	result = n1 * n2;
 
