43b7c936a2d8d892732bf946702d94f7690b82ec
Ilnnn <camoreze@gmail.com>
2025-11-11 17:39:08 +0000

    save of 3-alloc_grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+int **alloc_grid(int width, int height)
+{
+int **tab;
+int l = 0;
+int c = 0;
+int i = 0;
+
+if (width <= 0)
+return NULL;
+
+if (height <= 0)
+return NULL;
+
+tab = malloc(height * sizeof(int *));
+if (tab == NULL)
+return NULL,
+
+l = 0;
+
+while (l < height)
+{
+tab[l] = malloc(width * sizeof(int));
+if (tab[l] == NULL)
+i = 0;
+l++;
+}
+
+while (i < l)
+{
+free (tab[l]);
+i++;
+free(tab);
+return NULL;
+
+	c = 0;
+	while (c < width)
+	{
+		tab[l][c] = 0;
+		c++;
+	}
+	
+	l++;
+}
+return (tab);
+
+}
