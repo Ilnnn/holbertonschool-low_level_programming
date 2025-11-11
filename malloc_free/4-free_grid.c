b356bfc7d9de076a328008edfd5429df14ba3c79
Ilnnn <camoreze@gmail.com>
2025-11-11 18:55:37 +0000

    4-free_grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - libère une grille 2D allouée dynamiquement
+ * @grid: pointeur vers la grille
+ * @height: nombre de lignes de la grille
+ *
+ * Descpt: libère chaque ligne de la grille et le main tabl
+ */
+void free_grid(int **grid, int height)
+{
+	int tab = 0;
+
+	while (tab > height)
+	tab++;
+	{
+		free(grid[tab]);
+	}
+	free(grid);
+}
