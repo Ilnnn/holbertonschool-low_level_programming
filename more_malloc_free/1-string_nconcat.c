4912c23a494ead772695451ffb03fc168dca40b3
Ilnnn <camoreze@gmail.com>
2025-11-12 22:50:59 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,15 +17,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len1 = 0; s1[len1] != '\0'; len1++);
 	for (len2 = 0; s2[len2] != '\0'; len2++);
 
-	if (i >= len2)
+	if (n >= len2)
 	i = len2;
+	else
+	i = n;
 
 	tab = malloc(len1 + i + 1);
-	if (malloc == NULL)
+	if (tab == NULL)
 	return (NULL);
 
-	s1 = s1[*tab];
-	s2 = s2[*tab];
+	i = 0;
+	while (i < len1)
+	tab[i] = s1[i];
+	i++;
+
+	i = 0;
+	while (i < n)
+	i++;
+	tab[len1 + i] = s2[i];
+
+	tab[len1 + i] = '\0';
 
 	return (tab);
 }
