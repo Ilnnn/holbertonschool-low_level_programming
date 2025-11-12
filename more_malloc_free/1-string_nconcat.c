0243a3e45239e70461d9516145b1cd739f2fd3fc
Ilnnn <camoreze@gmail.com>
2025-11-12 23:07:33 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,6 +7,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
 	char *tab;
+	unsigned int tct = 0;
 
 	if (s1 == NULL)
 	s1 = "";
@@ -14,29 +15,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 	s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++);
-	for (len2 = 0; s2[len2] != '\0'; len2++);
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	if (n >= len2)
-	i = len2;
+	tct = len2;
 	else
-	i = n;
+	tct = n;
 
-	tab = malloc(len1 + i + 1);
+	tab = malloc(len1 + tct + 1);
 	if (tab == NULL)
 	return (NULL);
 
 	i = 0;
 	while (i < len1)
-	tab[i] = s1[i];
-	i++;
+	{
+		tab[i] = s1[i];
+		i++;
+	}
 
 	i = 0;
-	while (i < n)
-	i++;
-	tab[len1 + i] = s2[i];
+	while (i < tct)
+	{
+		tab[len1 + i] = s2[i];
+		i++;
+	}
 
 	tab[len1 + i] = '\0';
-
 	return (tab);
 }
