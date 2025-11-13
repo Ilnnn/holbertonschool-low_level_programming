dfd9b332a772d656319c248e65488d4c01a5d3dc
Ilnnn <camoreze@gmail.com>
2025-11-13 15:02:47 +0000

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	return;
+
+	if (d->name != NULL)
+	malloc(d->name)
+
+	if(d->owner != NULL)
+	malloc (d->owner)
+
+	malloc
+}
