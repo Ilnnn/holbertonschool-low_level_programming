978fe7fd484e744b49360c585e4924fc18290b80
Ilnnn <camoreze@gmail.com>
2025-11-13 11:06:22 +0000

    brouillon of 3-array

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+int *array_range(int min, int max)
+{
+	unsigned int i = 0;
+	unsigned int count = 0;
+	if (min > max)
+	return (NULL);
+
+	count = (max - min) + 1
+	count = malloc(min + max + 1)
+	if (count == NULL);
+	return (NULL);
+
+	i = 0;
+	while (i > 0)
+
+
+
+}
