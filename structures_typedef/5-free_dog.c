3680ca880e8c469c37c7f1ae968c4a71fcee37f4
Ilnnn <camoreze@gmail.com>
2025-11-14 14:17:57 +0000

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,16 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_dog - libère une structure dog
+ * @d: pointeur vers la structure à libérer
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 	return;
 
 	if (d->name != NULL)
-	malloc(d->name)
+	free(d->name);
 
-	if(d->owner != NULL)
-	malloc (d->owner)
+	if (d->owner != NULL)
+	free(d->owner);
 
-	malloc
+	free(d);
 }
