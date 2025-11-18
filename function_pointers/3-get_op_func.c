842ffd7c168da2311678b7962b3b1b84b547a575
Ilnnn <camoreze@gmail.com>
2025-11-18 10:52:29 +0000

    files of T3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "functions_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
