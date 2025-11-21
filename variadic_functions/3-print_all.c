76989bb965983ac2f51c0883257af3eb6ddd2764
Ilnnn <camoreze@gmail.com>
2025-11-21 19:12:01 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,45 +1,70 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
+
+void print_char(va_list list)
+
+{
+	char c = va_arg(list, int);
+	printf("%c", c);
+}
+
+void print_int(va_list list)
+{
+	int i = va_arg(list, int);
+	printf("%i", i);
+}
+
+void print_float(va_list list)
+{
+	float f = va_arg(list, double);
+	printf("%f", f);
+}
+
+void print_string(va_list list)
+{
+
+char *s = va_arg(list, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
 
 void print_all(const char * const format, ...)
 {
-   va_list args;
-   char c;
-   int i;
-   int first = 1;
-   const char *p;
+	unsigned int i = 0, j;
+	char *separator = "";
+	va_list list;
 
-	va_start(args, format);
-	p = format;
+	type_txt_t type[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string},
+	{0, NULL},
+						};
 
-	while (p && *p)
-{
-    if (!first)
+	va_start(list, format);
+
+	while (format && format[i] != '\0')
 	{
-		printf(", ");
-	}
-
-	if (*p == 'c' || *p == 'i')
+	j = 0;
+	while (type[j].type != 0)
 	{
-	
-		if (*p == 'c')
-    
+	if (type[j].type == format[i])
 		{
-			c = (char)va_arg(args, int);
-			printf("%c", c);
-        }
-
-        if (*p == 'i')
-
-		{
-		i = va_arg(args, int);
-		printf("%d", i);
+			printf("%s", separator);
+			type[j].print(list);
+			separator = ", ";
 		}
-			first = 0;
+		j++;
 	}
-			p++;
-}
-	
-	va_end(args);
-	printf("\n");
+		i++;
+	}
+
+va_end(list);
+printf("\n");
 }
