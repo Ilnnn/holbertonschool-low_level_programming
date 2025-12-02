792cf4c7977fb3870657ee1c6f30b47c592bada0
Ilnnn <camoreze@gmail.com>
2025-12-02 11:06:47 +0000

    first attempt of 2-add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t count = 0;
+
+	if (str == NULL || head == NULL)
+	return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (malloc == NULL)
+	return (NULL);
+}
