8e28e88db58614108d95bed226e34e1e33da8b61
Ilnnn <camoreze@gmail.com>
2025-12-04 09:39:26 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,26 +9,21 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-
 	mew2->n = n;
-	mew2->prev = NULL;
 	mew2->next = NULL;
 
 	if (*head == NULL)
 	{
-		head = mew2;
+		mew2->prev = NULL;
+		*head = mew2;
 		return (mew2);
 	}
 
 	c = *head;
 	while (c->next != NULL)
-	{
+		{
 		c = c->next;
-	}
+		}
 		c->next = mew2;
 		mew2->prev = c;
 
