1b16f72305402e4c410b47377805e9f4790851bb
Ilnnn <camoreze@gmail.com>
2025-12-05 10:49:55 +0000

    1-create_file.c + betty correction

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,6 +4,13 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file to append to
+ * @text_content: NULL-terminated string to add at the end of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int titi, lnin, wrtt;
