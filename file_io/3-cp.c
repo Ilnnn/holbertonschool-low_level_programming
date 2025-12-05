e140c1a954828b4c11996d7ccc2744aff321d378
Ilnnn <camoreze@gmail.com>
2025-12-05 14:04:27 +0000

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t n_read;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		return (1);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+		{
+			write(fd_to, buffer, n_read);
+		}
+
+	close(fd_from);
+	close(fd_to);
+
+	return (0);
+}
